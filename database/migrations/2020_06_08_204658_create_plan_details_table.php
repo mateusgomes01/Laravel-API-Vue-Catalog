9ede49686beff51f29797b7fb969d07cc42711a0
mateusgomes01 <mgm6@cin.ufpe.br>
2020-06-08 18:22:37 -0300

    Created basic migrations

---
+++ database/migrations/2020_06_08_204658_create_plan_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlanDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plan_details', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('plan_id')->constrained();
+            $table->integer('installments');
+            $table->integer('visa_master');
+            $table->integer('others');
+            $table->integer('mup');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plan_details');
+    }
+}
